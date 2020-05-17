176d58defcd389b6b0eaab0a22e16df477e512a2
loisbr96 <lois.brandsema@student.hu.nl>
2020-05-17 20:36:13 +0200

    commented all out to test

--- src/main/java/com/persistence/model/Scoreboard.java
+++ src/main/java/com/persistence/model/Scoreboard.java
@@ -1,31 +1,31 @@
-package com.persistence.model;
-
-import javax.persistence.Column;
-
-public class Scoreboard {
-    @Column
-    private Game gameId;
-
-    @Column
-    private Game score;
-
-    @Column
-    private String name;
-
-    Scoreboard(){}
-
-    Scoreboard(Game gameId, Game score, String name){
-        this.gameId = gameId;
-        this.score = score;
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Game getScore() {
-        return score;
-    }
-
-}
+//package com.persistence.model;
+//
+//import javax.persistence.Column;
+//
+//public class Scoreboard {
+//    @Column
+//    private Game gameId;
+//
+//    @Column
+//    private Game score;
+//
+//    @Column
+//    private String name;
+//
+//    Scoreboard(){}
+//
+//    Scoreboard(Game gameId, Game score, String name){
+//        this.gameId = gameId;
+//        this.score = score;
+//        this.name = name;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public Game getScore() {
+//        return score;
+//    }
+//
+//}
