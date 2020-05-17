424a2945c105415ac9f993a0f58762fb820061f5
loisbr96 <lois.brandsema@student.hu.nl>
2020-05-17 11:36:40 +0200

    commended out other trys

--- src/main/java/com/persistence/model/Word.java
+++ src/main/java/com/persistence/model/Word.java
@@ -1,27 +1,27 @@
-package com.persistence.model;
-
-public class Word {
-    private int id;
-    private String word;
-
-    public Word(int id, String word){
-        this.id = id;
-        this.word = word;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getWord() {
-        return word;
-    }
-
-    public void setWord(String word) {
-        this.word = word;
-    }
-}
+//package com.persistence.model;
+//
+//public class Word {
+//    private int id;
+//    private String word;
+//
+//    public Word(int id, String word){
+//        this.id = id;
+//        this.word = word;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getWord() {
+//        return word;
+//    }
+//
+//    public void setWord(String word) {
+//        this.word = word;
+//    }
+//}
