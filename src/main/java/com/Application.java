45316064d3c64d7e5c7c8aeaa3113cc27869718d
loisbr96 <lois.brandsema@student.hu.nl>
2020-05-24 21:01:28 +0200

    3e

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,12 +1,24 @@
 package com;
 
+import com.persistence.WordRepository;
+import com.persistence.model.Word;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
+
+//    @Bean
+//    ApplicationRunner applicationRunner(WordRepository wordRepository){
+//        return args -> {
+//            wordRepository.save(new Word("testen"));
+//            wordRepository.save(new Word("checken"));
+//        };
+//    }
 
 }
