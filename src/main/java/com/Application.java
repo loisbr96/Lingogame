2dfad9375033d53760f60d25c0f23a9a9a67a6fe
loisbr96 <lois.brandsema@student.hu.nl>
2020-05-24 16:19:01 +0200

    Added Spring profiles the correct way, so that de database can be run local and on heroku

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,12 +1,7 @@
 package com;
 
-import com.persistence.WordRepository;
-import com.persistence.model.Word;
-import com.web.controller.WordController;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
--- src/main/java/com/persistence/model/Word.java
+++ src/main/java/com/persistence/model/Word.java
@@ -1,7 +1,6 @@
 package com.persistence.model;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 public class Word {
